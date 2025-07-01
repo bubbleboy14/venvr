715bfea81f9e0ac074abcc28304525fc6bca2735
bubbleboy14 <mario.balibrera@gmail.com>
2025-07-01 03:33:41 -0700

    1st cut

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='venvr',
+    version="0.1",
+    author='Mario Balibrera',
+    author_email='mario.balibrera@gmail.com',
+    license='MIT License',
+    description='Virtual ENVironment manageR',
+    long_description='classes and functions for interacting with virtual environments',
+    packages=[
+        'venvr'
+    ],
+    zip_safe = False,
+    install_requires = [
+        "fyg >= 0.1.5"
+    ],
+    entry_points = '''''',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
