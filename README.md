d460997de71528dcc003b90842090313f720fd92
bubbleboy14 <mario.balibrera@gmail.com>
2025-11-13 20:43:58 -0800

    vpush 0.1.5.8

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Virtual ENVironment manageR
 
-v0.1.5.7
+v0.1.5.8
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='venvr',
-    version="0.1.5.7",
+    version="0.1.5.8",
     author='Mario Balibrera',
     author_email='mario.balibrera@gmail.com',
     license='MIT License',
